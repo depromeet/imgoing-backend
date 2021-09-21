d0b47600aa6ab8caac4f0b4aedb3fdce36bdcbf6
Jungwoo Song <37579681+bluayer@users.noreply.github.com>
2021-09-21 17:30:05 +0900

    CI, CD setting (#1)
    * feat: set for eb
    
    * fix: naming 변경
    
    * fix: remove jpa
    
    * fix: remove plain.jar
    
    * fix: application name error fixed

--- src/main/java/org/imgoing/api/controller/HealthCheckController.java
+++ src/main/java/org/imgoing/api/controller/HealthCheckController.java
@@ -1,17 +1,16 @@
 package org.imgoing.api.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
     @GetMapping("")
-    @ResponseBody
-    public String healthCheck() {
+    public ResponseEntity<String> healthCheck() {
         String message = "Welcome to imgoing API! :)";
-        return message;
+        return ResponseEntity.status(200).body(message);
     }
 }
