05e70d5d5514fab9ab0b67447ff7a9dc9e9c336a
Jungwoo Song <37579681+bluayer@users.noreply.github.com>
2021-10-02 14:06:31 +0900

    feat: 서술형으로 변경

--- src/main/java/org/imgoing/api/support/ImgoingError.java
+++ src/main/java/org/imgoing/api/support/ImgoingError.java
@@ -5,9 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ImgoingError {
-    UNKNOWN_ERROR("알 수 없는 에러", HttpStatus.INTERNAL_SERVER_ERROR),
-    NOT_LOGIN("비 로그인 사용자", HttpStatus.UNAUTHORIZED),
-    ENTITY_NOT_FOUND("엔터티를 찾을 수 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    UNKNOWN_ERROR("알 수 없는 에러입니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    NOT_LOGIN("비 로그인 사용자입니다.", HttpStatus.UNAUTHORIZED),
     BAD_REQUEST("사용자의 입력이 잘못되었습니다.", HttpStatus.BAD_REQUEST),
     NOT_PERMITTED("권한이 없는 유저 입니다.", HttpStatus.FORBIDDEN),
     DUPLICATE("중복 입력입니다.", HttpStatus.BAD_REQUEST),
