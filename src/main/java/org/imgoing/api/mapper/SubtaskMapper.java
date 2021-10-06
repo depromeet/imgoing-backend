4c68b522f34551131d35df08fa8294649813ae5b
황종훈 <ybell1028@gmail.com>
2021-10-06 23:03:17 +0900

    Add : Subtask에 isLast 컬럼 추가, 응답시 Dto로 변환 과정 추가, 기획내용처럼 세부항목 -> 준비항목으로 명칭 변경

--- src/main/java/org/imgoing/api/mapper/SubtaskMapper.java
+++ src/main/java/org/imgoing/api/mapper/SubtaskMapper.java
@@ -9,12 +9,7 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SubtaskMapper {
-    @Mapping(target = "id", expression = "java(subtask.getId())")
-    @Mapping(target = "name", expression = "java(subtask.getName())")
-    @Mapping(target = "time", expression = "java(subtask.getTime())")
     SubtaskDto toDto(Subtask subtask);
 
-    @Mapping(target = "name", expression = "java(dto.getName())")
-    @Mapping(target = "time", expression = "java(dto.getTime())")
     Subtask toEntity(SubtaskDto dto);
 }
