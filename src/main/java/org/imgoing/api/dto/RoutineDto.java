4fb86848e36d5d2e4afbfca021a18b8b3ba8c0a1
황종훈 <ybell1028@gmail.com>
2021-10-07 10:39:03 +0900

    Feat : 루틴 RUD 추가, Task와 관계 삭제

--- src/main/java/org/imgoing/api/dto/RoutineDto.java
+++ src/main/java/org/imgoing/api/dto/RoutineDto.java
@@ -7,9 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.imgoing.api.entity.Routine;
-import org.imgoing.api.entity.Subtask;
-import org.imgoing.api.entity.Task;
 
 import java.util.List;
 
@@ -24,9 +21,6 @@ public class RoutineDto {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Create {
-        @ApiModelProperty(value = "업무 Id")
-        private Long taskId;
-
         @ApiModelProperty(value = "준비항목 목록 Id")
         private List<Long> subtaskIdList;
     }
@@ -39,9 +33,6 @@ public class RoutineDto {
     public static class Read {
         @ApiModelProperty(value = "루틴 Id")
         private Long id;
-
-        @ApiModelProperty(value = "업무 모델")
-        private TaskDto Task;
 
         @ApiModelProperty(value = "준비항목 모델")
         private List<SubtaskDto> subtaskList;
