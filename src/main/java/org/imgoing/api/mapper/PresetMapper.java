9697b7b6fe70742605f79c48592587508361ab11
황종훈 <ybell1028@gmail.com>
2021-10-09 19:53:37 +0900

    Fix : Task -> Preset으로 엔티티 이름 변경

--- src/main/java/org/imgoing/api/mapper/PresetMapper.java
+++ src/main/java/org/imgoing/api/mapper/PresetMapper.java
@@ -1,15 +1,15 @@
 package org.imgoing.api.mapper;
 
-import org.imgoing.api.dto.TaskDto;
-import org.imgoing.api.domain.entity.Task;
+import org.imgoing.api.dto.PresetDto;
+import org.imgoing.api.domain.entity.Preset;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", // 스프링 컨테이너에 객체로 관리
         unmappedTargetPolicy = ReportingPolicy.IGNORE) // 일치하지 않는 필드를 무시
 
-public interface TaskMapper {
-    TaskDto toDto(Task task);
+public interface PresetMapper {
+    PresetDto toDto(Preset preset);
 
-    Task toEntity(TaskDto dto);
+    Preset toEntity(PresetDto dto);
 }
