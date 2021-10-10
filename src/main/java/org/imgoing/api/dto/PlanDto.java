742cc9b977ae20541c87b694009c15d0025dadc6
ujin2021 <ujin2021@naver.com>
2021-10-10 18:17:01 +0900

    Fix: Plan 엔티티, 생성, 조회, 삭제 수정

--- src/main/java/org/imgoing/api/dto/PlanDto.java
+++ src/main/java/org/imgoing/api/dto/PlanDto.java
@@ -13,27 +13,27 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 public class PlanDto {
+    @ApiModelProperty(value = "일정 id")
+    private Long id;
+
     @ApiModelProperty(value = "일정 이름")
     private String name;
 
-    @ApiModelProperty(value = "출발 장소 이름")
-    private String departure_name;
-
-    @ApiModelProperty(value = "출발 위도")
-    private Double departure_lat;
-
-    @ApiModelProperty(value = "출발 경도")
-    private Double departure_lng;
-
     @ApiModelProperty(value = "도착 장소 이름")
-    private String arrival_name;
+    private String arrivalName;
 
     @ApiModelProperty(value = "도착 위도")
-    private Double arrival_lat;
+    private Double arrivalLat;
 
     @ApiModelProperty(value = "도착 경도")
-    private Double arrival_lng;
+    private Double arrivalLng;
 
     @ApiModelProperty(value = "도착 시간")
-    private LocalDateTime arrival_at;
+    private LocalDateTime arrivalAt;
+
+    @ApiModelProperty(value = "메모")
+    private String memo;
+
+    @ApiModelProperty(value = "챙길 물건들")
+    private String belongings;
 }
