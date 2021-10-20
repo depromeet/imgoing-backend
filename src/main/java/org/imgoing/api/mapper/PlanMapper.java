8e4dd84bfcaedecfead07dd76e7206177262318e
ujin2021 <ujin2021@naver.com>
2021-10-20 21:49:59 +0900

    Fix: toDto 함수에 taskDto 추가

--- src/main/java/org/imgoing/api/mapper/PlanMapper.java
+++ src/main/java/org/imgoing/api/mapper/PlanMapper.java
@@ -2,13 +2,16 @@ package org.imgoing.api.mapper;
 
 import org.imgoing.api.dto.PlanDto;
 import org.imgoing.api.domain.entity.Plan;
+import org.imgoing.api.dto.TaskDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PlanMapper {
-    PlanDto toDto(Plan plan);
+    PlanDto toDto(Plan plan, List<TaskDto> taskDtos);
 
     Plan toEntity(PlanDto planDto);
 }
