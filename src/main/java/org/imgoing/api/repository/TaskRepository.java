d5ec9b30e8a9e1ddce00733cb62bed85d562eb44
황종훈 <ybell1028@gmail.com>
2021-10-27 04:00:54 +0900

    Refactor : TaskRepository.findAllByUserId JPQL로 변경

--- src/main/java/org/imgoing/api/repository/TaskRepository.java
+++ src/main/java/org/imgoing/api/repository/TaskRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    @Query(value = "SELECT * FROM task_tb WHERE task_tb.user_id = :userId", nativeQuery = true)
+    @Query(value = "SELECT t FROM Task t WHERE t.user.id = :userId")
     List<Task> findAllByUserId(Long userId);
 
     List<Task> findAllByIdIn(List<Long> taskIds);
