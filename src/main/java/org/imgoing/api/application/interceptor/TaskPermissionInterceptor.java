2381f4ff5d5e22349ceddf95673ad72cd384701b
황종훈 <ybell1028@gmail.com>
2021-10-30 21:33:20 +0900

    Feat : Routine, Task 접근 시 권한 체크 기능 추가

---
+++ src/main/java/org/imgoing/api/application/interceptor/TaskPermissionInterceptor.java
@@ -0,0 +1,46 @@
+package org.imgoing.api.application.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.imgoing.api.domain.entity.CertificateAuthority;
+import org.imgoing.api.domain.entity.Task;
+import org.imgoing.api.domain.vo.TokenPayload;
+import org.imgoing.api.service.TaskService;
+import org.imgoing.api.support.ImgoingError;
+import org.imgoing.api.support.ImgoingException;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class TaskPermissionInterceptor implements HandlerInterceptor {
+    private final TaskService taskService;
+    private final CertificateAuthority certificateAuthority;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String requestHttpMethod = request.getMethod();
+        Map<?, ?> pathVariables = (Map<?, ?>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+
+        if( (HttpMethod.GET.matches(requestHttpMethod) && !pathVariables.isEmpty())
+                || HttpMethod.PUT.matches(requestHttpMethod)
+                || HttpMethod.DELETE.matches(requestHttpMethod) ) {
+            TokenPayload payload = certificateAuthority.decrypt(request.getHeader("x-access-token"));
+            String[] path = taskService.getClass().getSuperclass().toString().split("\\.");
+            String entityName = path[path.length - 1].toLowerCase().replace("service", "");
+
+            Long id = Long.parseLong((String)pathVariables.get(entityName + "Id"));
+
+            Task data = taskService.getById(id);
+            if(data.getUser().getId() == payload.getId()) {
+                return true;
+            }
+            throw new ImgoingException(ImgoingError.BAD_REQUEST, "접근할 수 없는 컨텐츠 입니다.");
+        } else return true;
+    }
+}
