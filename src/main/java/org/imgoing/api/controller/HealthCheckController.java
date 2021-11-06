c5e2333c8114e4d33ccf6a9ba411bff9f205e1a7
황종훈 <ybell1028@gmail.com>
2021-11-06 18:00:12 +0900

    Add : 개인정보 처리방침 추가

--- src/main/java/org/imgoing/api/controller/HealthCheckController.java
+++ src/main/java/org/imgoing/api/controller/HealthCheckController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/")
@@ -12,5 +13,10 @@ public class HealthCheckController {
     public ResponseEntity<String> healthCheck() {
         String message = "Welcome to imgoing API! :)";
         return ResponseEntity.status(200).body(message);
+    }
+
+    @GetMapping("/policy")
+    public ModelAndView privacyPolicy() {
+        return new ModelAndView("privacy-policy");
     }
 }
