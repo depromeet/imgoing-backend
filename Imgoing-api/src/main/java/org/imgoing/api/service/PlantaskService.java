fd61f747e2ab2ffac82a79f8c5f27484b8819f18
Jungwoo Song <37579681+bluayer@users.noreply.github.com>
2021-11-20 16:24:39 +0900

    feat: multimodule init

--- Imgoing-api/src/main/java/org/imgoing/api/service/PlantaskService.java
+++ Imgoing-api/src/main/java/org/imgoing/api/service/PlantaskService.java
@@ -1,19 +1,14 @@
 package org.imgoing.api.service;
 
 import lombok.RequiredArgsConstructor;
-import org.imgoing.api.domain.entity.Plan;
 import org.imgoing.api.domain.entity.Plantask;
-import org.imgoing.api.domain.entity.Task;
-import org.imgoing.api.mapper.PlantaskMapper;
 import org.imgoing.api.repository.PlantaskRepository;
 import org.imgoing.api.support.ImgoingError;
 import org.imgoing.api.support.ImgoingException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
---
+++ Imgoing-push/src/main/java/org/imgoing/push/PushApplication.java
@@ -0,0 +1,12 @@
+package org.imgoing.push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushApplication.class, args);
+    }
+}
