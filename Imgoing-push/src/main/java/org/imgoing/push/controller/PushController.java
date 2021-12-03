4a9b7b0a236c7b38ea00c850a63390bc1c658b27
황종훈 <ybell1028@gmail.com>
2021-12-04 01:11:22 +0900

    Add : Topic으로 전송하는 컨트롤러 추가

--- Imgoing-push/src/main/java/org/imgoing/push/controller/PushController.java
+++ Imgoing-push/src/main/java/org/imgoing/push/controller/PushController.java
@@ -3,8 +3,9 @@ package org.imgoing.push.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.imgoing.push.dto.FcmDto;
-import org.imgoing.push.service.FcmService;
+import org.imgoing.push.dto.FcmTokenDto;
+import org.imgoing.push.dto.FcmTopicDto;
+import org.imgoing.push.service.FcmServerProtocolClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +18,21 @@ import java.io.IOException;
 @Api(tags = "푸시 관련 API")
 @RequestMapping("/push/v2")
 public class PushController {
-    private final FcmService fcmService;
+    private final FcmServerProtocolClient fcmServerProtocolClient;
 
-    @ApiOperation(value = "유저에게 푸시 전송")
+    @ApiOperation(value = "토큰으로 푸시 전송")
     @PostMapping
-    public void send(@RequestBody FcmDto request) throws IOException {
-        fcmService.send(request.getTokens(),
+    public void sendToToken(@RequestBody FcmTokenDto request) throws IOException {
+        fcmServerProtocolClient.sendByToken(request.getTokens(),
+                request.getNotification().getTitle(),
+                request.getNotification().getBody()
+        );
+    }
+
+    @ApiOperation(value = "토픽으로 푸시 전송")
+    @PostMapping("/topic")
+    public void sendByTopic(@RequestBody FcmTopicDto request) throws IOException {
+        fcmServerProtocolClient.sendByTopic(request.getTopic(),
                 request.getNotification().getTitle(),
                 request.getNotification().getBody()
         );
