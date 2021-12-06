2dbf64d0ea28a64e6f52c6c46ab412d794948b0e
bluayer <ijacsong98@gmail.com>
2021-12-06 13:12:39 +0900

    fix: log 삭제

--- Imgoing-api/src/main/java/org/imgoing/api/service/UserService.java
+++ Imgoing-api/src/main/java/org/imgoing/api/service/UserService.java
@@ -20,7 +20,6 @@ public class UserService {
     public long[] getMonthlyUserStats (User user) {
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime startOfMonth = now.minusDays(now.getDayOfMonth()-1).withMinute(0).withHour(0).withSecond(0).withNano(0);
-        System.out.println(startOfMonth);
         List<Plan> planOneMonthAgo = planRepository.findAllByUserAndActualArrivalAtGreaterThanEqual(user, startOfMonth);
         long totalPlanCount = planOneMonthAgo.size();
         long latePlanCount = planOneMonthAgo.stream().filter(Plan::getIsUserLate).count();
